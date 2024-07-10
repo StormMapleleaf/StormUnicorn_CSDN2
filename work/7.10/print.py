0c60a26ba559bf7209f4791817a1da50267c0692
dai_qi_xiang <1290023174@qq.com>
2024-07-10 14:03:36 +0800

    update

--- work/7.10/print.py
+++ work/7.10/print.py
@@ -1,15 +1,11 @@
 def p(n):
     print("*"*n, end="")
 
-def square(n,m):
-    for i in range(n):
-        if i == 0 or i == n-1:
-            p(m)
-            print()
+def square(n,m):         print()
         else:
             print("*", end="")
             for k in range(m-2):
                 print(" ", end="")
             print("*")
 
-square(8,18)
+square( 8,18)
