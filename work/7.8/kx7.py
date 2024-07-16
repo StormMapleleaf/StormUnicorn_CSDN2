33c0347cfa95df908d6629e439a32d944080fe14
dai_qi_xiang <1290023174@qq.com>
2024-07-16 14:05:57 +0800

    update

--- work/7.8/kx7.py
+++ work/7.8/kx7.py
@@ -1,12 +1,13 @@
-print("   *", end="\n")
-print("  *", end="")
-print(" *", end="\n")
-print(" *", end="")
-print("   *", end="\n")
-print("*", end="")
-print("     *", end="\n")
-print(" *", end="")
-print("   *", end="\n")
-print("  *", end="")
-print(" *", end="\n")
-print("   *", end="\n")
+# 归并排序
+from heapq import merge
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+print(merge_sort([3, 2, 1, 4, 5, 6, 7, 8, 9, 0])
